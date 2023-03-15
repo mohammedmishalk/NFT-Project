43db319a791eb862b5a9eaff5ca488cc0c7ef2b4
mohammedmishalk <mishalnunu@gmail.com>
2023-03-15 15:58:49 +0530

    project started

---
+++ nft/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+# from django.shortcuts import render 
+
+class NftConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nft'
+
